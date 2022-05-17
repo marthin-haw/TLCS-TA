48e47c19596b484b53d511e54948b3e092b5210a
marthin-haw <marthinhawila32000@gmail.com>
2022-05-17 22:28:54 +0700

    program untuk mengkonversi data csv menjadi file .rou

--- datakendaraan.py
+++ datakendaraan.py
@@ -1,11 +1,37 @@
-
 import csv
+import pandas as pd
 
-with open('data_kendaraan.csv') as f1:
+with open('C:\\Users\\marth\\Documents\\Tugas Akhir\\YOLO dan OpenCV\\vehicle-detection-classification-opencv\\data1-sort.csv') as f1:
     data = csv.reader(f1)
-    for row in data:
-        id = row[0]
-        route = row[1]
-        depart = row[2]
-        departSpeed = row[3]
-        print("    <vehicle id=\"vehicle_" + id + "\" depart=\"" + depart + "\" departLane=\"random\" departSpeed=\"" + departSpeed + "\" route=\"route_" + route +"\"/>")
+
+    with open("intersection/mapTA2new.rou.xml", "w") as routes:
+        print('''<routes>
+        <!-- VTypes -->
+        <vType id="DEFAULT_VEHTYPE" emissionClass="HBEFA3/Bus" guiShape="bus" color="red"/>
+        <vType id="bus" vClass="bus" emissionClass="HBEFA3/PC_G_EU4" guiShape="bus" width="1.80" height="1.50" color="blue"/>
+        <vType id="car" emissionClass="HBEFA3/PC_G_EU4" guiShape="passenger" width="1.80" height="1.50" color="red"/>
+        <vType id="motorcycle" vClass="motorcycle" emissionClass="HBEFA3/PC_G_EU4" guiShape="motorcycle" width="1.80" height="1.50" color="green"/>
+        <vType id="truck" vClass="truck" emissionClass="HBEFA3/PC_G_EU4" guiShape="truck" width="1.80" height="1.50" color="yellow"/>
+        <!-- Routes -->
+        <route id="route_0" edges="W2TL TL2E" color="yellow"/>
+        <route id="route_1" edges="W2TL TL2S" color="yellow"/>
+        <route id="route_10" edges="N2TL TL2W" color="yellow"/>
+        <route id="route_11" edges="N2TL TL2E" color="yellow"/>
+        <route id="route_2" edges="W2TL TL2N" color="yellow"/>
+        <route id="route_3" edges="S2TL TL2N" color="yellow"/>
+        <route id="route_4" edges="S2TL TL2W" color="yellow"/>
+        <route id="route_5" edges="S2TL TL2E" color="yellow"/>
+        <route id="route_6" edges="E2TL TL2W" color="yellow"/>
+        <route id="route_7" edges="E2TL TL2S" color="yellow"/>
+        <route id="route_8" edges="E2TL TL2N" color="yellow"/>
+        <route id="route_9" edges="N2TL TL2S" color="yellow"/>
+        <!-- Vehicles, persons and containers (sorted by depart) -->''', file=routes)
+        for row in data:
+            if row:
+                id = row[0]
+                class_name = row[1]
+                route = row[2]
+                depart = row[3]
+                print("    <vehicle id=\"" + id + "\" type=\"" + class_name + "\" depart=\"" + depart + "\" departLane=\"random\" departSpeed=\"10\" route=\"" + route + "\"/>", file=routes)
+
+        print("</routes>", file=routes)
